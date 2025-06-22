5255f2cfb346e9b17df3f8c8eda8a23e2cc338c5
androbelgica <androbelgica85@gmail.com>
2025-06-22 22:08:10 +0800

    Fix: use HTTPS in asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ["resources/css/app.css", "resources/js/app.jsx"], // adjust as needed
             refresh: true,
         }),
-        react(),
     ],
+    server: {
+        https: true, // for local dev only
+    },
+    build: {
+        manifest: true,
+        outDir: "public/build",
+    },
 });
