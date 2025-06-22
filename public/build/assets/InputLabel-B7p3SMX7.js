f2d2b9f826e167bdfdf2e9ffb04ac05e21ccaec8
androbelgica <androbelgica85@gmail.com>
2025-06-22 22:27:22 +0800

    Include Vite build output for deployment

---
+++ public/build/assets/InputLabel-B7p3SMX7.js
@@ -0,0 +1,1 @@
+function n({value:t,className:e="",children:a,...r}){return React.createElement("label",{...r,className:"block text-sm font-medium text-gray-700 dark:text-gray-300 "+e},t||a)}export{n as I};
