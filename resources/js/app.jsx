f37d4c1e38e5a59a3d9f1fdab67f7504aaa7afa8
androbelgica <135511469+androbelgica@users.noreply.github.com>
2025-06-23 14:11:35 +0800

    re build

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import "../css/app.css";
 import "./bootstrap";
 import "./echo";
 
+import React from 'react';
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
